359e608eca0dbe01cd3cd51a92df6ff1896eff36
Kemal DAVUT <mustafakemaldavut@gmail.com>
2022-08-09 12:05:03 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+const form = document.querySelector("#create-task-form");
+const Tasks = document.querySelector("#new-task-description");
+const addTasks = document.querySelector("#tasks");
+const deleteTasks = document.querySelector("#delete-tasks");
+const editTasks = document.querySelector("#edit-tasks");
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const task = Tasks.value;
+    const newTask = document.createElement("li");
+    newTask.textContent = task;
+    addTasks.appendChild(newTask);
+    Tasks.value = "";
+    
+} );
+
+deleteTasks.addEventListener("click", (event) => {
+    event.preventDefault();
+    addTasks.innerHTML = "";
+} );
+} );
